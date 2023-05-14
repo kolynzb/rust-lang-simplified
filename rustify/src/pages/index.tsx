8a56f6409c1b18a4283395109d2bb0e75f8f22ed
kolynzb <collinsbenda360@gmail.com>
2023-05-14 21:08:50 +0300

    feat: add hero to home section

--- rustify/src/pages/index.tsx
+++ rustify/src/pages/index.tsx
@@ -6,23 +6,14 @@ import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import styles from "./index.module.css";
+import HomeHero from "../components/HomeHero";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Rust Essentials Fast and Easy ⏱️
-          </Link>
-        </div>
-      </div>
+    <header className={clsx("hero ", styles.heroBanner)}>
+
+      <HomeHero />
     </header>
   );
 }
