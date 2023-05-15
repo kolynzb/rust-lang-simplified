adb6fffb13368be225b82f1b321221cb4bb8dcde
kolynzb <collinsbenda360@gmail.com>
2023-05-15 19:44:11 +0300

    feat: add github cta to home

--- rustify/src/pages/index.tsx
+++ rustify/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -7,12 +7,17 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import styles from "./index.module.css";
 import HomeHero from "../components/HomeHero";
+import HomeBanner from "../components/HomeBanner";
+import { useLocation } from "@docusaurus/router";
+import useIsMobile from "../hooks/UseIsMobile";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import GitHubFloatingCTA from "../components/GitHubFloatingCTA";
+import { GithubProvider } from "../context/GithubContext";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero ", styles.heroBanner)}>
-
       <HomeHero />
     </header>
   );
@@ -20,6 +25,32 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+  const location = useLocation();
+
+  const isMobile = useIsMobile();
+
+  const showGithubCta = useMemo(() => {
+    if (typeof window !== "undefined" && !isMobile) {
+      if (location.pathname.startsWith("/blog")) {
+        (window as Window & typeof globalThis & { Intercom?: any }).Intercom?.(
+          "update",
+          { hide_default_launcher: true }
+        );
+      } else {
+        (window as Window & typeof globalThis & { Intercom?: any }).Intercom?.(
+          "update",
+          { hide_default_launcher: false }
+        );
+      }
+    }
+
+    if (location.pathname.startsWith("/docs")) {
+      return false;
+    }
+
+    return true;
+  }, [location, isMobile]);
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -28,7 +59,13 @@ export default function Home(): JSX.Element {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <HomeBanner />
       </main>
+      <GithubProvider>
+        {showGithubCta && (
+          <BrowserOnly>{() => <GitHubFloatingCTA />}</BrowserOnly>
+        )}
+      </GithubProvider>
     </Layout>
   );
 }
