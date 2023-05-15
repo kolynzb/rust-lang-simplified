23c4c7734378445eb7bf38e13f5fcd894ed6426d
kolynzb <collinsbenda360@gmail.com>
2023-05-15 11:05:06 +0300

    style: lint

--- rustify/src/components/HomepageFeatures/index.tsx
+++ rustify/src/components/HomepageFeatures/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import Translate from "@docusaurus/Translate";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -20,8 +21,8 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Focus on What Matters",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -30,8 +31,8 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Powered by React",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -41,9 +42,9 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -55,6 +56,101 @@ function Feature({title, Svg, description}: FeatureItem) {
   );
 }
 
+function Feature2({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={styles.cardsGroupContainer}>
+      <div className={styles.cardsGroup}>
+        <div className={styles.cardsGroupSection}>
+          <strong>
+            <Translate>It&apos;s a</Translate>
+          </strong>
+          <div className={styles.title}>
+            <p>
+              <Translate>Bridge between Cloud and IDE</Translate>
+            </p>
+          </div>
+          <p className={styles.detail}>
+            <Translate>
+              Code changes can instantly synchronize from IDE to cloud, see
+              changes under seconds, without image commit, push or pull cycles.
+            </Translate>
+          </p>
+          <div className={styles.icon}>
+            <img src="img/home/section2-images-1.png"></img>
+          </div>
+        </div>
+        <div
+          className={styles.cardsGroupSection}
+          style={{ backgroundColor: "#0f2b34" }}
+        >
+          <strong>
+            <Translate>It&apos;s a</Translate>
+          </strong>
+          <div className={styles.title}>
+            <p>
+              <Translate>Space to Share Dev Environment</Translate>
+            </p>
+          </div>
+          <p className={styles.detail}>
+            <Translate>
+              Developers can share the same development environment across the
+              team, enjoying team collaboration.
+            </Translate>
+          </p>
+          <div className={styles.icon}>
+            <img src="img/home/section2-images-2.png"></img>
+          </div>
+        </div>
+        <div
+          className={styles.cardsGroupSection}
+          style={{ backgroundColor: "#0f2b34" }}
+        >
+          <strong>
+            <Translate>It&apos;s a</Translate>
+          </strong>
+          <div className={styles.title}>
+            <p>
+              <Translate>Extension Toolsets of Kubernetes</Translate>
+            </p>
+          </div>
+          <p className={styles.detail}>
+            <Translate>
+              Nocalhost can help developers to build Kubernetes applications
+              much easier and faster.
+            </Translate>
+          </p>
+          <div className={styles.icon}>
+            <img src="img/home/section2-images-3.png"></img>
+          </div>
+        </div>
+        <div
+          className={clsx(styles.cardsGroupSection3, styles.cardsGroupSection)}
+          style={{ backgroundColor: "#fff" }}
+        >
+          <strong>
+            <Translate>It&apos;s a</Translate>
+          </strong>
+          <div className={styles.title}>
+            <p>
+              <Translate>Spotlight On Explore MicroServices</Translate>
+            </p>
+          </div>
+          <p className={styles.detail}>
+            <Translate>
+              Developers can directly develop and debug remote designated
+              microservices under the complex framework through Nocalhost.
+            </Translate>
+          </p>
+          <div className={styles.icon}>
+            <img src="img/home/section2-images-4.png"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// https://github.dev/nocalhost/nocalhost.github.io
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
