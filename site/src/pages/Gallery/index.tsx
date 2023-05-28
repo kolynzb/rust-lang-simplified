b74fa9e51f6d93f9e523053e501821ab47a50ec3
kolynzb <collinsbenda360@gmail.com>
2023-05-28 18:47:31 +0300

    feat: add submit button

--- site/src/pages/Gallery/index.tsx
+++ site/src/pages/Gallery/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Heading from "@theme/Heading";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
@@ -7,33 +8,65 @@ import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
 import { translate } from "@docusaurus/Translate";
 import Layout from "@theme/Layout";
+import styles from "./styles.module.css";
+import { TbUpload } from "react-icons/tb";
 
-const TITLE = translate({ message: "Docusaurus Site Showcase" });
+const TITLE = translate({ message: "Community Gallery" });
 const DESCRIPTION = translate({
-  message: "List of websites people are building with Docusaurus",
+  message: "Images From Our Community",
 });
-// https://www.lightgalleryjs.com/docs/react/
+
 const Gallery: React.FC = () => {
   return (
     <Layout>
-      <section className="gallery">
-        <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
-          <a href="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80">
-            <img
-              alt="img1"
-              src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-            />
-          </a>
-          <a href="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80">
-            <img
-              alt="img2"
-              src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-            />
-          </a>
-        </LightGallery>
+      <section className="margin-top--lg margin-bottom--lg text--center">
+        <Heading as="h1">{TITLE}</Heading>
+        <p>{DESCRIPTION}</p>
+        <section className="gallery">
+          <LightGallery
+            speed={500}
+            plugins={[lgThumbnail, lgZoom]}
+            autoplay={true}
+          >
+            <a href="https://source.unsplash.com/1600x900/?hackathon,ideas,community,people,technology,conference">
+              <img
+                alt="community1"
+                className={styles.gallery__img}
+                src="https://source.unsplash.com/1600x900/?hackathon,ideas,community,people,technology,conference"
+              />
+            </a>
+            <a href="https://source.unsplash.com/1600x900/?community,people,technology,conference">
+              <img
+                className={styles.gallery__img}
+                alt="community2"
+                src="https://source.unsplash.com/1600x900/?community,technology,conference"
+              />
+            </a>
+            <a href="https://source.unsplash.com/1600x900/?hackathon,ideas,technology,conference">
+              <img
+                alt="community1"
+                className={styles.gallery__img}
+                src="https://source.unsplash.com/1600x900/?hackathon,ideas,technology,conference"
+              />
+            </a>
+            <a href="https://source.unsplash.com/1600x900/?community,people,technology,conference">
+              <img
+                className={styles.gallery__img}
+                alt="community2"
+                src="https://source.unsplash.com/1600x900/?community,people,technology,conference"
+              />
+            </a>
+          </LightGallery>
+        </section>
+
+        <button title="Submit Image" className={styles.submit_img__btn}>
+          <TbUpload size={24} color="white" />
+        </button>
       </section>
     </Layout>
   );
 };
 
 export default Gallery;
+
+// https://www.lightgalleryjs.com/docs/react/
